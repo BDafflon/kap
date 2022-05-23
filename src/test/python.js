bf5846fb95210be4af330b4c9f01a93187dd4a2e
BDafflon <trysac@gmail.com>
2022-05-23 13:29:22 +0200

    Update python.js

--- src/test/python.js
+++ src/test/python.js
@@ -22,8 +22,9 @@ const [rep, setRep]= useState();
 const hightlightWithLineNumbers = (input, language) =>{
   console.log("hightlightWithLineNumbers",input,language)
   if(input==="") input="\n"
-  return highlight(input, language)
-    .split("\n")
+  var hcode =  highlight(input, language)
+    console.log("hcode",hcode)
+    return hcode.split("\n")
     .map((line, i) => `<span class='editorLineNumber'>${i + 1}</span>${line}`)
     .join("\n");
 }
